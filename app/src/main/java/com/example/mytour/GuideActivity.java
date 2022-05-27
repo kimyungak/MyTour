57b6013014062c1b251b1bc1cf908cb4606a715f
robot <kimyungak>
2022-05-27 10:31:44 +0900

    test

--- app/src/main/java/com/example/mytour/GuideActivity.java
+++ app/src/main/java/com/example/mytour/GuideActivity.java
@@ -2,7 +2,10 @@ package com.example.mytour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        findViewById(R.id.img_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Intent intent = new Intent(Intent.ACTION_VIEW);
+                //       Uri.parse("https://www.naver.com");
+                //              startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q=경기"))
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mytour/InfoActivity.java
+++ app/src/main/java/com/example/mytour/InfoActivity.java
@@ -9,7 +9,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
+    int no = 0;
+    int imgs[] = {R.drawable.info1, R.drawable.info2, R.drawable.info3, R.drawable.info4, R.drawable.info5};
 
+    String txts[] = {"1번설명","2번설명","3번설명","4번설명","5번설명"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +21,21 @@ public class InfoActivity extends AppCompatActivity {
         ImageView img_info = findViewById(R.id.img_info);
         TextView txt_info = findViewById(R.id.txt_info);
 
+
+
         img_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(InfoActivity.this, "클릭", Toast.LENGTH_SHORT).show(); 
+                Toast.makeText(InfoActivity.this, "클릭", Toast.LENGTH_SHORT).show();
+                no = ++no % 5;
+                img_info.setImageResource(imgs[no]);
+                txt_info.setText(txts[no]);
+            }
+        });
+        findViewById(R.id.btn_info_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
